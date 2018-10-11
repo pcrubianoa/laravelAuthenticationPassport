d8211620f89d2552789c94e446f36bf6177ed56b
pcrubianoa <pcrubianoa@gmail.com>
2018-10-11 18:14:18 -0500

    Tweet timeline

---
+++ resources/js/components/Timeline.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Timeline</div>
+
+                    <div class="card-body">
+                        <div class="media" v-for="tweet in tweets" v-bind:key="tweet.id">
+                            <div class="media-left mr-3">
+                                <a href="#">
+                                    <img width="30" src="http://cdn.onlinewebfonts.com/svg/img_87237.png" alt="User Avatar">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <strong>{{ tweet.user.name }}</strong>
+                                <p>{{ tweet.body }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                tweets: []
+            }
+        },
+        mounted() {
+            this.$http.get('/tweets').then((response) => {
+                this.tweets = response.body
+            })
+        }
+    }
+</script>
