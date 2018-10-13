03842c7261ab88195262d9494c07accb453c1aa3
pcrubianoa <pcrubianoa@gmail.com>
2018-10-12 22:22:02 -0500

    Using personal access token

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class TweetController extends Controller
 {
     public function index(Request $request)
     {
-        return $request->user()->tweets()->with(['user'])->get();
+        return $request->user()->tweets()->with(['user'])->latestFirst()->get();
     }
 
     public function store(Request $request){
